b000c833705691166582a667e9319e49a8ee7ef7
juanst13 <60800254+juanst13@users.noreply.github.com>
2021-07-17 17:26:56 -0500

    Add project files.

---
+++ FinalUno/FinalUno.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace FinalUno.Skia.Gtk
+{
+}
---
+++ FinalUno/FinalUno.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace FinalUno.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
